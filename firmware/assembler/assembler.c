1509e360a3bdd257a87090fa1835f1e1cf8b0739
tyler-ottman <flet2060@gmail.com>
2022-05-01 22:58:29 -0700

    Added preliminary assembler code

--- firmware/assembler/assembler.c
+++ firmware/assembler/assembler.c
@@ -4,27 +4,77 @@
 
 #define INSTRUCTION_LENGTH 10
 
-char** instructions;
-int num_instructions;
+struct reg_pair {
+    char* name;
+    char* pseudo_name;
+};
 
-void initialize(FILE* fd) {
+struct Architecture {
+    char** instruction_types;
+    struct reg_pair* registers;  
+    int num_instructions;
+    int num_registers;
+};
+
+struct Architecture risc;
+
+void remove_spaces(char* s) {
+    char* d = s;
+    do {
+        while (*d == ' ' || *d == '\t' || *d == '\n') {
+            ++d;
+        }
+    } while (*s++ = *d++);
+}
+
+void initialize() {
+    FILE* fd = fopen("./arch-spec/instructions.txt", "r");
+    if (fd == NULL) {
+        printf("Error: could not open instructions.txt");
+        exit(EXIT_FAILURE);
+    }
+    risc.num_instructions = 0;
     size_t buf_size = INSTRUCTION_LENGTH;
     char* buffer = (char*)malloc(buf_size * sizeof(char));
 
+    // Read Instructions types
     while (getline(&buffer, &buf_size, fd) != -1)
-        num_instructions++;
+        risc.num_instructions++;
     rewind(fd);
 
-    instructions = (char**)malloc(num_instructions * sizeof(char*));
-    for (int i = 0; i < num_instructions; i++) {
-        instructions[i] = (char*)malloc(INSTRUCTION_LENGTH * sizeof(char));
+    risc.instruction_types = (char**)malloc(risc.num_instructions * sizeof(char*));
+    for (int i = 0; i < risc.num_instructions; i++) {
+        risc.instruction_types[i] = (char*)malloc(INSTRUCTION_LENGTH * sizeof(char));
         getline(&buffer, &buf_size, fd);
-        buffer[strlen(buffer)-1] = '\0';
-        strncpy(instructions[i], buffer, INSTRUCTION_LENGTH - 1);
+        remove_spaces(buffer);
+        strcpy(risc.instruction_types[i], buffer);
     }
 
-    for (int i = 0; i < num_instructions; i++) {
-        printf("%s\n", instructions[i]);
+    // Read Register names
+    fd = fopen("./arch-spec/registers.txt", "r");
+    if (fd == NULL) {
+        printf("Error: Could not open registers.txt\n");
+        exit(EXIT_FAILURE);
+    }
+
+    risc.num_registers = 0;
+    while (getline(&buffer, &buf_size, fd) != -1)
+        risc.num_registers++;
+    rewind(fd);
+
+    risc.registers = (struct reg_pair*)malloc(risc.num_registers * sizeof(struct reg_pair));
+    char* name = (char*)malloc(strlen(buffer) * sizeof(char));
+    char* pseudo_name = (char*)malloc(strlen(buffer) * sizeof(char));
+    char* delim; // pointer to comma
+    for (int i = 0; i < risc.num_registers; i++) {
+        // Get name, pseudo-name pair
+        getline(&buffer, &buf_size, fd);
+        remove_spaces(buffer);
+        delim = strchr(buffer , ',');
+        risc.registers[i].name = (char*)malloc(strlen(buffer)*sizeof(char));
+        risc.registers[i].pseudo_name = (char*)malloc(strlen(buffer)*sizeof(char));
+        strncpy(risc.registers[i].name, buffer, (int)(delim - buffer));
+        strcpy(risc.registers[i].pseudo_name, ++delim);
     }
 }
 
@@ -33,13 +83,8 @@ int main(int argc, char** argv) {
     if (argc == 1) {
         printf("Error: asm file not found\n");
         exit(EXIT_FAILURE);
-    } else if (argc == 2) {
-        FILE* fd = fopen("instructions.txt", "r");
-        if (fd == NULL) {
-            printf("Error: could not open %s\n", argv[1]);
-            exit(EXIT_FAILURE);
-        }
-        initialize(fd);
     }
-    // exit(1);
+    initialize();
+
+    exit(EXIT_SUCCESS);
 }
