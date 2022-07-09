10700923376d7daef499783e6f40a315bb6c1e53
tyler-ottman <flet2060@gmail.com>
2022-07-08 18:46:24 -0700

    Added dynamic memory allocation support

--- game-firmware/src/bootup.c
+++ game-firmware/src/bootup.c
@@ -3,6 +3,8 @@ typedef unsigned int uint32_t;
 extern uint32_t _program_end;
 extern uint32_t _sdata;
 extern uint32_t _edata;
+extern uint32_t _sbss;
+extern uint32_t _ebss;
 
 void load_ram(void) {
     uint32_t* rom_data = &_program_end;     // Points to data to load into RAM from ROM
@@ -11,4 +13,26 @@ void load_ram(void) {
     while (ram_ptr < &_edata) {
         *ram_ptr++ = *rom_data++;
     }
+
+    rom_data = &_sbss;
+    while (rom_data < &_ebss) {              // Unitialized data
+        *rom_data++ = 0;
+    }
+}
+
+// code from https://sourceware.org/newlib/libc.html#Syscalls
+char *_sbrk(uint32_t incr)
+{
+    extern char _heapbase; /* Defined by the linker */
+    static char *heap_end;
+    char *prev_heap_end;
+
+    if (heap_end == 0)
+    {
+        heap_end = &_heapbase;
+    }
+    prev_heap_end = heap_end;
+
+    heap_end += incr;
+    return (char *)prev_heap_end;
 }
--- game-firmware/src/game.c
+++ game-firmware/src/game.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #define VRAM_START 0x40000000
 #define USELESS_VAR 0x50000000
 volatile unsigned int *VIDEO_MEMORY = (unsigned int *)(VRAM_START);
@@ -10,6 +12,7 @@ volatile unsigned int *VIDEO_MEMORY = (unsigned int *)(VRAM_START);
 #define BLUE 0xffff0000
 
 int c = USELESS_VAR;
+int unused_arr[20];
 void colorScreen(volatile unsigned int *video_mem, unsigned int color)
 {
     // for (int i = 0; i < WIDTH * HEIGHT; i++) {
@@ -62,24 +65,32 @@ void colorGradient(volatile unsigned int *video_mem)
 // Code set up for WIDTHxHEIGHT pixel monitor with VGA reading from 0x40000000
 int main(void)
 {
-    while (1)
-    {
-        // colorScreen(VIDEO_MEMORY, BLUE);
-        // colorScreen(VIDEO_MEMORY, GREEN);
+    // while (1)
+    // {
+    //     // colorScreen(VIDEO_MEMORY, BLUE);
+    //     // colorScreen(VIDEO_MEMORY, GREEN);
         
-        for (int i = 0; i < 10; i++)
-            colorScreen(VIDEO_MEMORY, GREEN);
-        // colorScreen(VIDEO_MEMORY, RED);
-        for (int i = 0; i < 10; i++)
-            colorScreen(VIDEO_MEMORY, 0x0);
-        for (int i = 0; i < 10; i++)
-            colorScreen(VIDEO_MEMORY, BLUE);
-        for (int i = 0; i < 10; i++)
-            colorScreen(VIDEO_MEMORY, 0xff00ffff);
+    //     for (int i = 0; i < 10; i++)
+    //         colorScreen(VIDEO_MEMORY, GREEN);
+    //     // colorScreen(VIDEO_MEMORY, RED);
+    //     for (int i = 0; i < 10; i++)
+    //         colorScreen(VIDEO_MEMORY, 0x0);
+    //     for (int i = 0; i < 10; i++)
+    //         colorScreen(VIDEO_MEMORY, BLUE);
+    //     for (int i = 0; i < 10; i++)
+    //         colorScreen(VIDEO_MEMORY, 0xff00ffff);
         
-        // colorGradient(VIDEO_MEMORY);
-        // drawImage(VIDEO_MEMORY);
-        // break;
+    //     // colorGradient(VIDEO_MEMORY);
+    //     // drawImage(VIDEO_MEMORY);
+    //     // break;
+    // }
+    int* test_arr = (int*)malloc(10*sizeof(int));
+    for (int i = 0; i < 10; i++) {
+        test_arr[i] = -1; // 0xFFFFFFFF
+    }
+    int* test_arr2 = (int*)malloc(5*sizeof(int));
+    for (int i = 0; i < 5; i++) {
+        test_arr2[i] = 0xABCDABCD;
     }
     while(1){}
     return 0;
