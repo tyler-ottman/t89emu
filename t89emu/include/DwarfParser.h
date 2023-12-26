9dc917a5ba3fcb03b4d3f51b936e7b9068e2233b
tyler-ottman <flet2060@gmail.com>
2023-12-26 00:07:59 -0800

    Added Compile Unit class generation paired with Abbreviation tables

--- t89emu/include/DwarfParser.h
+++ t89emu/include/DwarfParser.h
@@ -14,37 +14,94 @@ struct BaseUnitHeader {
     uint32_t debugAbbrevOffset;
 };
 
-struct CompileUnitHeader {
-    struct BaseUnitHeader;
-};
+class AttributeEntry {
+public:
+    AttributeEntry(size_t name, size_t form, size_t special);
+    ~AttributeEntry();
 
-struct AttributeEntry {
+    size_t getName(void);
+    size_t getForm(void);
+    size_t getSpecial(void);
+
+private:
     size_t name;
     size_t form;
     size_t special;
 };
 
-struct AbbrevEntry {
-    size_t code;
-    size_t tag;
+class AbbrevEntry {
+public:
+    AbbrevEntry(size_t dieCode, size_t dieTag, bool hasChild);
+    ~AbbrevEntry();
+
+    void addAttributeEntry(AttributeEntry *attEntry);
+
+    size_t getNumAttributes(void);
+    AttributeEntry *getAttributeEntry(size_t index);
+    size_t getDieCode(void);
+    size_t getDieTag(void);
+    bool hasChildren(void);
+
+private:
+    size_t dieCode;
+    size_t dieTag;
     bool hasChild;
-    std::vector<AttributeEntry *> atEntries;
+
+    std::vector<AttributeEntry *> attEntries;
+};
+
+class AbbrevTable {
+public:
+    AbbrevTable(uint8_t *abbrevTableStart);
+    ~AbbrevTable();
+
+    AbbrevEntry *getAbbrevEntry(size_t dieCode);
+
+private:
+    std::map<size_t, AbbrevEntry *> abbrevEntries;
+};
+
+// Compile Unit Header in .debug_info
+class CompileUnitHeader {
+public:
+    CompileUnitHeader(uint8_t *debugInfoCompileUnitHeader);
+    ~CompileUnitHeader();
+
+    uint32_t getUnitLength(void);
+    uint16_t getUnitVersion(void);
+    uint8_t getUnitType(void);
+    uint8_t getAddressSize(void);
+    uint32_t getDebugAbbrevOffset(void);
+
+private:
+    struct FullCompileUnitHeader {
+        struct BaseUnitHeader base;
+    };
+
+    FullCompileUnitHeader header;
+};
+
+class CompileUnit {
+public:
+    CompileUnit(uint8_t *debugInfoCompileUnitHeader, uint8_t *debugAbbrevStart);
+    ~CompileUnit();
+
+    size_t getLength(void);
+
+private:
+    CompileUnitHeader *compileUnitHeader;
+
+    // Compile Unit Abbreviation Table
+    AbbrevTable *abbrevTable;
 };
 
 class DwarfParser : public ElfParser {
 public:
     DwarfParser(const char *fileName);
     ~DwarfParser();
-
-    void generateAbbrevTable(void);
     
 private:
-    size_t decodeLeb128(const uint8_t **data);
-
-    std::map<size_t, AbbrevEntry *> abbrevTable;
-
-    const ElfSectionHeader *debugAbbrev;
-    const ElfSectionHeader *debugInfo;
+    std::vector<CompileUnit *> compileUnits;
 };
 
 #endif // DWARFPARSER_H
--- t89emu/src/DwarfParser.cpp
+++ t89emu/src/DwarfParser.cpp
@@ -1,61 +1,6 @@
 #include "DwarfParser.h"
 
-DwarfParser::DwarfParser(const char *fileName) : ElfParser::ElfParser(fileName) {
-    debugInfo = getSectionHeader(".debug_info");
-    ASSERT(debugInfo, ".debug_info not found\n");
-
-    debugAbbrev = getSectionHeader(".debug_abbrev");
-    ASSERT(debugAbbrev, ".debug_abbrev not found\n");
-
-    generateAbbrevTable();
-}
-
-DwarfParser::~DwarfParser() {
-
-}
-
-void DwarfParser::generateAbbrevTable() {
-    const uint8_t *abbrevData = elfFileInfo->elfData + debugAbbrev->offset;
-
-    for (;;) {
-        if (((uintptr_t)abbrevData -
-             (uintptr_t)(elfFileInfo->elfData + debugAbbrev->offset)) >=
-            debugAbbrev->size) {
-            break;
-        }
-
-        AbbrevEntry *abbrevEntry = new AbbrevEntry();
-
-        abbrevEntry->code = decodeLeb128(&abbrevData);
-        if (abbrevEntry->code == 0) { // End of Compile Unit Abbreviation
-            delete abbrevEntry;
-            continue;
-        }
-
-        abbrevEntry->tag = decodeLeb128(&abbrevData);
-        abbrevEntry->hasChild = *abbrevData++;
-
-        for (;;) {
-            size_t name = decodeLeb128(&abbrevData);
-            size_t form = decodeLeb128(&abbrevData);
-            if (name == 0 && form == 0) break;
-
-            AttributeEntry *atEntry = new AttributeEntry();
-            atEntry->name = name;
-            atEntry->form = form;
-            
-            if (atEntry->form == DW_FORM_implicit_const) {
-                atEntry->special = decodeLeb128(&abbrevData);
-            }
-
-            abbrevEntry->atEntries.push_back(atEntry);
-        }
-
-        abbrevTable.insert({abbrevEntry->code, abbrevEntry});
-    }
-}
-
-size_t DwarfParser::decodeLeb128(const uint8_t **data) {
+static size_t decodeLeb128(const uint8_t **data) {
     size_t result = 0;
     size_t shift = 0;
     
@@ -66,4 +11,171 @@ size_t DwarfParser::decodeLeb128(const uint8_t **data) {
     }
 
     return result;
+}
+
+AttributeEntry::AttributeEntry(size_t name, size_t form, size_t special)
+    : name(name), form(form), special(special) {}
+
+AttributeEntry::~AttributeEntry() {}
+
+size_t AttributeEntry::getName() {
+    return name;
+}
+
+size_t AttributeEntry::getForm() {
+    return form;
+}
+
+size_t AttributeEntry::getSpecial() {
+    return special;
+}
+
+AbbrevEntry::AbbrevEntry(size_t dieCode, size_t dieTag, bool hasChild)
+    : dieCode(dieCode), dieTag(dieTag), hasChild(hasChild) {}
+
+AbbrevEntry::~AbbrevEntry() {
+    for (AttributeEntry *attEntry : attEntries) {
+        delete attEntry;
+    }
+
+    attEntries.clear();
+}
+
+void AbbrevEntry::addAttributeEntry(AttributeEntry *attEntry) {
+    attEntries.push_back(attEntry);
+}
+
+size_t AbbrevEntry::getNumAttributes(void) {
+    return attEntries.size();
+}
+
+AttributeEntry *AbbrevEntry::getAttributeEntry(size_t index) {
+    if (index < 0 || index >= attEntries.size()) {
+        return nullptr;
+    }
+    return attEntries[index];
+}
+
+size_t AbbrevEntry::getDieCode() {
+    return dieCode;
+}
+
+size_t AbbrevEntry::getDieTag() {
+    return dieTag;
+}
+
+bool AbbrevEntry::hasChildren() {
+    return hasChild;
+}
+
+AbbrevTable::AbbrevTable(uint8_t *abbrevTableStart) {
+    const uint8_t *abbrevData = abbrevTableStart;
+
+    for (;;) {
+        size_t dieCode = decodeLeb128(&abbrevData);
+        if (dieCode == 0) { // End of Compile Unit
+            break;
+        }
+
+        size_t dieTag = decodeLeb128(&abbrevData);
+        size_t hasChild = decodeLeb128(&abbrevData);
+        AbbrevEntry *abbrevEntry = new AbbrevEntry(dieCode, dieTag, hasChild);
+
+        for (;;) {
+            size_t name = decodeLeb128(&abbrevData);
+            size_t form = decodeLeb128(&abbrevData);
+            if (name == 0 && form == 0) { break; }
+            size_t special = form == DW_FORM_implicit_const 
+                             ? decodeLeb128(&abbrevData) : 0;
+
+            abbrevEntry->addAttributeEntry(
+                new AttributeEntry(name, form, special));
+        }
+
+        abbrevEntries.insert({dieCode, abbrevEntry});
+    }
+}
+
+AbbrevTable::~AbbrevTable() {
+    for (auto const &abbrevEntry : abbrevEntries) {
+        delete abbrevEntry.second;
+    }
+    abbrevEntries.clear();
+}
+
+AbbrevEntry *AbbrevTable::getAbbrevEntry(size_t dieCode) {
+    return abbrevEntries.find(dieCode) != abbrevEntries.end()
+               ? abbrevEntries[dieCode]
+               : nullptr;
+}
+
+CompileUnitHeader::CompileUnitHeader(uint8_t *debugInfoCompileUnitHeader) {
+    memcpy(&header, debugInfoCompileUnitHeader, sizeof(FullCompileUnitHeader));
+}
+
+CompileUnitHeader::~CompileUnitHeader() {}
+
+uint32_t CompileUnitHeader::getUnitLength() {
+    return header.base.unitLength;
+}
+
+uint16_t CompileUnitHeader::getUnitVersion() {
+    return header.base.version;
+}
+
+uint8_t CompileUnitHeader::getUnitType() {
+    return header.base.unitType;
+}
+
+uint8_t CompileUnitHeader::getAddressSize() {
+    return header.base.addressSize;
+}
+
+uint32_t CompileUnitHeader::getDebugAbbrevOffset() {
+    return header.base.debugAbbrevOffset;
+}
+
+CompileUnit::CompileUnit(uint8_t *debugInfoCompileUnitHeader,
+                         uint8_t *debugAbbrevStart) {
+    compileUnitHeader = new CompileUnitHeader(debugInfoCompileUnitHeader);
+    abbrevTable = new AbbrevTable(debugAbbrevStart +
+                                  compileUnitHeader->getDebugAbbrevOffset());
+}
+
+CompileUnit::~CompileUnit() {
+    delete compileUnitHeader;
+    delete abbrevTable;
+}
+
+size_t CompileUnit::getLength() {
+    // size of length-field (4 bytes) + Rest of CU Header + DIEs
+    return 4 + compileUnitHeader->getUnitLength();
+}
+
+DwarfParser::DwarfParser(const char *fileName)
+    : ElfParser::ElfParser(fileName) {
+    const ElfSectionHeader *debugInfoHeader = getSectionHeader(".debug_info");
+    uint8_t *debugInfoCUHeader = elfFileInfo->elfData + debugInfoHeader->offset;
+    uint8_t *debugInfoEnd = debugInfoCUHeader + debugInfoHeader->size;
+    uint8_t *debugAbbrevStart =
+        elfFileInfo->elfData + getSectionHeader(".debug_abbrev")->offset;
+
+    // Initialize Compile Units
+    for (;;) {
+        // End of .debug_info section, CU parsing complete
+        if ((uintptr_t)debugInfoCUHeader >= ((uintptr_t)debugInfoEnd)) {
+            break;
+        }
+
+        CompileUnit *compileUnit =
+            new CompileUnit(debugInfoCUHeader, debugAbbrevStart);
+        compileUnits.push_back(compileUnit);
+
+        // Point to next CU Header
+        debugInfoCUHeader += compileUnit->getLength();
+    }
+}
+
+DwarfParser::~DwarfParser() {
+    
 }
